3561a853824a4f719bba13f171c71fdc07fae3f5
An Front <anya41791@yandex.ru>
2023-08-02 17:42:51 +0300

    try

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.listen(PORT);
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64c7a736f893c931ba5e5ed9',
+  };
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,12 +21,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64c7a736f893c931ba5e5ed9',
-  };
-  next();
-});
+
 
 app.use('/', require('./routes/cards'));
 app.use('/', require('./routes/users'));
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'НЕ туда :(('
+  })
+})
+
