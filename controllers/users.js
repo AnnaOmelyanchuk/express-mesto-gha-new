b72fa4333275f55767b7a9c0ce4f1ad5a8bb53d6
An Front <anya41791@yandex.ru>
2023-08-02 16:08:18 +0300

    try

--- controllers/users.js
+++ controllers/users.js
@@ -60,8 +60,9 @@ module.exports.updateUserInfo = (req, res) => {
 };
 
 module.exports.updateUserAvatar = (req, res) => {
+  console.log(req.user._id);
   Users.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar }, { new: true, runValidators: true })
-  then((user) => res.status(200).send({ data: user }))
+  .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
         return res.status(400).send({ message: 'Ошибка в данных' })
