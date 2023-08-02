c1dcfced69718d17b2648192929c7a1d5385ef58
An Front <anya41791@yandex.ru>
2023-08-02 10:36:47 +0300

    try

--- controllers/users.js
+++ controllers/users.js
@@ -26,8 +26,8 @@ module.exports.createUser = (req, res) => {
   })
     .then((users) => res.status(200).send({ data: users }))
     .catch((err) => {
-      if (true
-        // err instanceof mongoose.Error.CastError
+      if (
+        err instanceof mongoose.Error.CastError
       )
         return res.status(500).send({ message: 'Произошла ошибка' })
 
