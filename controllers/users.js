0c7406bd45332d026c89d95bd00cc46db754674a
An Front <anya41791@yandex.ru>
2023-08-04 12:51:50 +0300

    second_try

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,29 @@
 const { default: mongoose } = require('mongoose');
 const Users = require('../models/users');
 
+const BadRequesError = 400;
+const NotFoundError = 404;
+const ServerError = 500;
+
 module.exports.getUsers = (req, res) => {
   Users.find({})
-    .then((users) => res.status(200).send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(ServerError).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUserById = (req, res) => {
   Users.findById(req.params.userId)
     .then((user) => {
       if (user != null) {
-        return res.status(200).send({ data: user });
+        return res.send({ data: user });
       }
-      return res.status(404).send({ message: 'Нет такого id' });
+      return res.status(NotFoundError).send({ message: 'Нет такого id' });
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
-        return res.status(400).send({ message: 'Некорректный id' });
+        return res.status(BadRequesError).send({ message: 'Некорректный id' });
       }
-      return res.status(500).send({ message: 'Произошла ошибка' });
+      return res.status(ServerError).send({ message: 'Произошла ошибка' });
     });
 };
 
@@ -29,24 +33,24 @@ module.exports.createUser = (req, res) => {
     about: req.body.about,
     avatar: req.body.avatar,
   })
-    .then((users) => res.status(200).send({ data: users }))
+    .then((users) => res.send({ data: users }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(400).send({ message: 'Ошибка в данных' });
+        return res.status(BadRequesError).send({ message: 'Ошибка в данных' });
       }
-      return res.status(500).send({ message: 'Произошла ошибка' });
+      return res.status(ServerError).send({ message: 'Произошла ошибка' });
     });
 };
 
 module.exports.updateUserInfo = (req, res) => {
   const { name, about } = req.body;
   Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(400).send({ message: 'Ошибка в данных' });
+        return res.status(BadRequesError).send({ message: 'Ошибка в данных' });
       }
-      return res.status(500).send({ message: 'Произошла ошибка' });
+      return res.status(ServerError).send({ message: 'Произошла ошибка' });
     });
 };
 
@@ -56,11 +60,11 @@ module.exports.updateUserAvatar = (req, res) => {
     { avatar: req.body.avatar },
     { new: true, runValidators: true },
   )
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(400).send({ message: 'Ошибка в данных' });
+        return res.status(BadRequesError).send({ message: 'Ошибка в данных' });
       }
-      return res.status(500).send({ message: 'Произошла ошибка' });
+      return res.status(ServerError).send({ message: 'Произошла ошибка' });
     });
 };
